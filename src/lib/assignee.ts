00c1d5c2f32f60f4c738be3c65fe6fe9ea138a2c
BenOno <bentoki.1213@gmail.com>
2024-10-25 15:52:39 -0700

    Test

--- src/lib/assignee.ts
+++ src/lib/assignee.ts
@@ -1,14 +1,14 @@
 export type Assignee = {
   name: "pumpkin" | "dracula" | "ghost" | "hat" | "spider";
-  src: `/asignee/${string}.svg`;
+  src: `/assignee/${string}.svg`;
 };
 
 export const assigneeMap: Record<Assignee["name"], Assignee> = {
-  pumpkin: { name: "pumpkin", src: "/asignee/pumpkin.svg" },
-  dracula: { name: "dracula", src: "/asignee/dracula.svg" },
-  ghost: { name: "ghost", src: "/asignee/ghost.svg" },
-  hat: { name: "hat", src: "/asignee/hat.svg" },
-  spider: { name: "spider", src: "/asignee/spider.svg" },
+  pumpkin: { name: "pumpkin", src: "/assignee/pumpkin.svg" },
+  dracula: { name: "dracula", src: "/assignee/dracula.svg" },
+  ghost: { name: "ghost", src: "/assignee/ghost.svg" },
+  hat: { name: "hat", src: "/assignee/hat.svg" },
+  spider: { name: "spider", src: "/assignee/spider.svg" },
 };
 
 export const getUnassignedAssignee = (assignees: Assignee[]): Assignee[] => {
